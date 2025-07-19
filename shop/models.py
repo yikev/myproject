da1178c6e0656eaad71516e8389a63fda32d83de
yikev <yi.kevin@outlook.com>
2025-07-19 13:54:33 -0700

    feat: initial database setup. product, tag, and category models.

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    tags = models.ManyToManyField(Tag, related_name='products')
+
+    def __str__(self):
+        return self.name
